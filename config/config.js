44365e6700d232d47b38ad0f4316d914247d5160
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-05-13 19:58:14 +0530

    Sign in and setting JWT in cookies to be used further completed

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ module.exports = {
     development: {
       MONGO_URL:"mongodb+srv://harsh07bharvada:gelato%4007@crudmongodb-t7ak7.mongodb.net/crud-jwt?retryWrites=true&w=majority",
       port: process.env.PORT || 3000,
-      saltingRounds: 10
+      saltingRounds: 10,
+      jwtSecret:'kaboom'
     }
   }
--- routes/openRoutes.js
+++ routes/openRoutes.js
@@ -1,7 +1,12 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const openRouter = express.Router();
 const User = require('../models/user');
+const {development:{jwtSecret}} = require('../config/config');
 
+
+//Sign Up router
 openRouter.post('/signup',(req,res)=>{
     
     let result = {};
@@ -26,4 +31,64 @@ openRouter.post('/signup',(req,res)=>{
     
 });
 
+//Sign In router
+openRouter.post('/signin',(req,res)=>{
+
+    let result = {};
+    let status = 200; 
+    const {username,password} = req.body;
+    const signedUser = {username,password};
+    User.findOne({username: username},function(err,doc){
+
+        if(err)
+        {
+            status = 404;
+            result.status = status;
+            result.error = err;
+            res.status(result.status).send(result);
+        }
+        else
+        {
+            bcrypt.compare(password,doc.password)
+            .then(match=>{
+                if(match)
+                {
+                    const expiration = '2d';
+                    const token = generateToken(username,expiration);
+                    result.status = status;
+                    result.token = token;
+                    result.result = signedUser;
+                    res.status(result.status).cookie('token', token, {
+                        expires: new Date(Date.now() + expiration),
+                        secure: false, // set to true if your using https
+                        httpOnly: true,
+                      }).send(result);
+                }
+                else
+                {
+                    status = 401;
+                    result.status = status;
+                    result.error = 'Authentication Error';
+                    res.status(result.status).send(result);
+                }
+            })
+            .catch(err=>{
+                status = 500;
+                result.status = status;
+                result.error = err;
+                res.status(status).send(result);
+            });
+        }
+    });
+});
+
+//Generate Token
+const generateToken=(username,expiration)=>{
+
+    const payload = { username:username};
+    const options = { expiresIn :expiration};
+    const secret = jwtSecret;
+    return jwt.sign(payload, secret, options);
+
+}
 module.exports = openRouter;
