a9f2db3514ba7dc934b1fd3f5b629957b732882b
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-05-15 14:16:02 +0530

    Added UI files

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/',(req,res)=> {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 
